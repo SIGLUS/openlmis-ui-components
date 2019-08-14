bb54732d654ddd80fcd901ab8e622a1dfc5bbe9e
Baiyuan Wang <bywang@thoughtworks.com>
2019-08-14 15:21:06 +0800

    [Baiyuan] Use JTE to setup jenkins

---
+++ pipeline_config.groovy
@@ -0,0 +1,11 @@
+/*
+  This file will inherit the configurations set via:
+    https://github.com/SIGLUS/siglus-pipeline-libraries/blob/master/pipeline_config.groovy
+
+  This repository level configuration file should now add configs
+  that are specific to this repo, such as the use of gradle for build() and
+  unit_test()
+*/
+libraries{
+  dev_ui
+}
